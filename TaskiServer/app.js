f2aa40acbba12a2b11dd8693f72993b8fb62693a
Henry Aghaulor <henry.aghaulor@outlook.com>
2016-06-08 15:40:24 -0400

    Initial Commit

---
+++ TaskiServer/app.js
@@ -0,0 +1,12 @@
+﻿var app,
+	express = require("express");
+
+app = express();
+
+app.get("/", function (req, res) {
+	res.send("Hello World!");
+});
+
+app.listen(3000, function () {
+	console.log("Example app listening on port 3000!");
+});
